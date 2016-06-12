87cdaba3e52f8dab4e03010596aca4f33ad7191b
Nick Kirby <nrkirb@gmail.com>
2016-06-12 15:16:35 +0100

    enable migrations, add gig model, add genre model, update database

---
+++ GigHub.DataLayer/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace GigHub.DataLayer.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name{ get; set; }
+    }
+}
--- GigHub.Web/App_Start/IdentityConfig.cs
+++ GigHub.Web/App_Start/IdentityConfig.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using GigHub.DataLayer.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using GigHub.DataLayer.Models;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace GigHub.Web
 {
